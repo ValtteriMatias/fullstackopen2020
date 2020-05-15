f190214715929047696e3590a6d28ca5e0b5cad7
Valtteri Luodemäki <valtteri.luodemaki@Valtteri-MBP.home>
2020-05-15 15:24:09 +0300

    unicafe 1.7

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -6,17 +6,34 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [allFeedback, setAll] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
+  
 
   const handleGoodClick = () => {
     setGood(good + 1)
+    setAll(allFeedback + 1)
+    setTotal(total + 1)
+    setAverage((total+1)/(allFeedback+1))
+    setPositive((good/(allFeedback+1))*100)
+    
   }
 
   const handleNeutralClick = () => {
     setNeutral(neutral + 1)
+    setAll(allFeedback + 1)
+    setAverage(total/(allFeedback+1))
+    setPositive((good/(allFeedback+1))*100)
   }
 
   const handleBadClick = () => {
     setBad(bad + 1)
+    setAll(allFeedback + 1)
+    setTotal(total - 1)
+    setAverage(total-1/(allFeedback+1))
+    setPositive((good/(allFeedback+1))*100)
   }
 
   const Button = ({ onClick, text }) => (
@@ -32,7 +49,13 @@ const App = () => {
         <Button onClick={handleNeutralClick} text='neutral' />
         <Button onClick={handleBadClick} text='bad' />
       <h2>statistics</h2>
-      <p>good {good} <br /> neutral {neutral} <br /> bad {bad} </p> 
+      <p>good {good} <br /> 
+      neutral {neutral} <br /> 
+      bad {bad} <br /> 
+      all  {allFeedback} <br/>
+      average {average} <br/>
+      positive {positive} % <br/>
+      </p> 
     </div>
   )
 }
