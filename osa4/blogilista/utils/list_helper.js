6c908cfb0af3510475f36dcac326df199788581d
Valtteri Luodemäki <valtteri.luodemaki@Valtteri-MBP.home>
2020-06-04 21:36:21 +0300

    apufunkitoita ja yksikkötestejä 4.5

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -1,7 +1,35 @@
 const dummy = (blogs) => {
-  // ...
+  return 1
+}
+
+
+const totalLikes = (blogs) => {
+
+  const reducer = (accumulator, currentValue) => accumulator + currentValue.likes
+  return blogs.reduce(reducer, 0)
+
+}
+
+
+const favouriteBlog = (blogs) => {
+
+
+  const maxi = blogs.map(x => x.likes).sort()[0]
+  const top = blogs.find(x => x.likes === maxi)
+
+  const json = {
+    title: top.title,
+    author: top.author,
+    likes: top.likes
+  }
+
+  return json
+
+
 }
 
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
+  favouriteBlog
 }
