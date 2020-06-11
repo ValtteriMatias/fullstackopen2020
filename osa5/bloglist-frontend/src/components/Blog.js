e1538c67fb612c560103a69d1786eaf0d9e62f24
Valtteri Luodemäki <valtteri.luodemaki@Valtteri-MBP.home>
2020-06-11 21:08:23 +0300

    blogilistan frontend 5.7

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,32 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author} {blog.url} {blog.likes}
-  </div>
-)
+import React, {useState} from 'react'
+
+
+const Blog = ({ blog }) => {
+  const [show, setShow] = useState(false)
+  
+  const showWhenVisible = { display: show ? '' : 'none' }
+
+  const label = show ? 'Hide' : 'Show'
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    borderRadius: 5,
+    marginBotttom: 10
+  }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} <button onClick={() => {setShow(!show)}}> {label}</button>
+      <div style={showWhenVisible}>
+        {blog.author} <br/> {blog.url} <br/>{blog.likes} <button > Like </button>
+       </div>
+    </div>
+  )
+  
+}
 
 export default Blog
