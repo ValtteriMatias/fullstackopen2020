79df6ffadd05ad79d3c0ec46b6d4b3dadec9ec60
Valtteri Luodemäki <valtteri.luodemaki@Valtteri-MBP.home>
2020-06-11 22:19:55 +0300

    blogilistan frontend 5.9

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -153,7 +153,7 @@ const App = () => {
         <button onClick={() => setBlogFormVisible(false)}>cancel</button>
       </div>  
      <h2>blogs</h2>
-      {blogs.map(blog =>
+      {blogs.sort((a, b) => b.likes - a.likes).map(blog =>
         <Blog key={blog.id} blog={blog}  updateBlog={updateBlog}/>
       )}
     </div>
